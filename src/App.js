8c66cbd1c4f2d912b3e21da01f3cec03a778933e
Kamallochan Das <118735381+kamallochan2004@users.noreply.github.com>
2023-04-21 22:42:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Notes from "./components/Notes";
+import "./components/style.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React.JS Bootcamp</h1>
+      <p>
+        We learnt the basics of Web-development, JavaScript, JSX, React, React
+        DOM and created a project at the very end of the bootcamp as well.
+      </p>
+    </div>
+  );
+}
+export default Notes;
